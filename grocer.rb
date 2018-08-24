340a0d984202002413d866b116192ad2425440a8
Nicky Liu <nickyliu91@gmail.com>
2018-08-24 08:14:49 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,18 +1,45 @@
 def consolidate_cart(cart)
   new_hash = {}
+  count = 0
   cart.each do |hash|
-
     hash.each do |item, info|
-      info.each do |info2, answer|
-        new_hash[item][info2] = answer
-      end
+      new_hash[item] ||= info
+
+      new_hash[item][:count] ||= 0
+      new_hash[item][:count] += 1
     end
   end
-  puts new_hash
+  new_hash
+
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupon_item = []
+  coupons.each do |hash|
+    coupon_item << [hash[:item], hash[:cost], hash[:num]]
+  end
+
+  coupon_item.each do |item|
+    unless cart.include?(item[0])
+      coupon_item.delete(item)
+    end
+  end
+
+  coupon_item.each do |item|
+    cart["#{item[0]} W/COUPON"] ||= {}
+    if cart.keys.include?(item)
+
+      cart["#{item[0]} W/COUPON"][:count] = 2
+    else
+      puts cart.keys
+      cart["#{item[0]} W/COUPON"][:clearance] = cart[item[0]][:clearance]
+      cart["#{item[0]} W/COUPON"][:price] = item[1]
+      cart[item[0]][:count] -= item[2]
+      cart["#{item[0]} W/COUPON"][:count] = 1
+    end
+  end
+
+  cart
 end
 
 def apply_clearance(cart)
@@ -21,4 +48,5 @@ end
 
 def checkout(cart, coupons)
   # code here
+
 end
