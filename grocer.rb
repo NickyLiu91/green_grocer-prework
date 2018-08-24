f26a242ca93a129f7a7925ac650b7850b8fd08b6
Nicky Liu <nickyliu91@gmail.com>
2018-08-24 09:27:34 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,13 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |hash|
+    hash.each do |item, info_hash|
+      if info_hash[:clearance] == true
+        info_hash[:price] = info_hash[:price] * 0.8
+      end
+    end
+  end
 end
 
 def checkout(cart, coupons)
